f8628166c8490f80e0cfe46887af811fa27ff594
leninceiba <47401291+leninceiba@users.noreply.github.com>
2019-10-02 17:27:37 -0500

    feat: version variable is created and initialized by accessing the package.json variable to use in the template.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {default as data} from '../../package.json';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'features-angular_web';
+  version = `V_${data.version}`;
 }
